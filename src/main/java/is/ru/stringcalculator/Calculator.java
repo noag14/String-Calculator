0c797724d9ebee152c497a4a25dd130a0b40732a
Noa Gudjonsdottir <noagudjons@gmail.com>
2015-10-16 23:34:37 +0000

    test new lines again

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
 		if (text.equals(""))
 			return 0;
 		else if(text.contains(",|n")){
-        	String[] numbers = text.split(",");
+        	String[] numbers = text.split(",|n");
         	int total = 0;
         	for(String number : numbers){
 		    	total += toInt(number);
