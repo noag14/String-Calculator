766c3ca51d32354d428ee029d6adbf041e1399bb
Noa Gudjonsdottir <noagudjons@gmail.com>
2015-10-17 09:06:52 +0000

    Handle a different delimiter

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,8 +6,9 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 			}
-		else if(text.contains(",|n"))
+		else if(text.contains(",")||text.contains("//")){
 			return sum(splitNumbers(text));
+			}
 		else
 			return 1;
 	}
@@ -17,7 +18,15 @@ public class Calculator {
 	}
 	
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
+		String delimiter = ",|n";
+		if (numbers.startsWith("//")){
+			int delimiterIndex = numbers.indexOf("//") + 2; 
+			delimiter = delimiter + numbers.substring(delimiterIndex, delimiterIndex + 1); 
+			numbers = numbers.substring(numbers.indexOf("n") + 1); 
+			return numbers.split(delimiter);
+		}
+		else {
+			return numbers.split(",|n");
 	}
       
     private static int sum(String[] numbers){
